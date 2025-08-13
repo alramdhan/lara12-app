1b93662d064fe96c3440de79d1e366eda417c562
Dika <al.ramdhan312@gmail.com>
2025-08-13 15:46:24 +0700

    update dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
                   <p class="mb-0">Password <b>secret</b></p>
                 </div>
                 <div class="card-body">
-                  <form role="form" method="POST" action="/session">
+                  <form role="form" method="POST" action="/dashboard">
                     @csrf
                     <label>Email</label>
                     <div class="mb-3">
@@ -78,24 +78,7 @@
       </section>
     </main>
 
-    <!-- Github buttons -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
     <script src="../assets/js/core/popper.min.js"></script>
-    <script src="../assets/js/core/bootstrap.min.js"></script>
-    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-    <script src="../assets/js/plugins/fullcalendar.min.js"></script>
-    <script src="../assets/js/plugins/chartjs.min.js"></script>
-    <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
-        var options = {
-            damping: '0.5'
-        }
-        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-        }
-    </script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::post('/dashboard', function () {
+    return view('home.dashboard');
+})->name('dashboard');
